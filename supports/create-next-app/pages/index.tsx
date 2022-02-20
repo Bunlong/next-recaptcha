5fa48d840d26b5ec424521f259332f33a198a7eb
Bunlong <bunlong.van@gmail.com>
2022-02-21 00:26:17 +0700

    Update supports

--- supports/create-next-app/pages/index.tsx
+++ supports/create-next-app/pages/index.tsx
@@ -1,24 +1,26 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
-import { ReCAPTCHAProvider } from 'next-recaptcha';
+import { ReCAPTCHAProvider, ReCAPTCHA } from 'next-recaptcha';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <ReCAPTCHAProvider
-        reCAPTCHAKey='[Your recaptcha key]'
+        reCAPTCHAKey=''
         // language='[optional_language]'
         isReCAPTCHANet={false}
         isEnterprise={false}
         script={{
-          async: false, // optional, default to false,
-          defer: false, // optional, default to false
-          appendTo: 'head', // optional, default to 'head', can be 'head' or 'body',
-          nonce: undefined // optional, default undefined
+          async: false,
+          defer: false,
+          appendTo: 'head',
+          nonce: undefined,
         }}
       >
-        Hello World
+        <ReCAPTCHA onVerify={token => {
+          console.log(token);
+        }} />
       </ReCAPTCHAProvider>
     </div>
   )
